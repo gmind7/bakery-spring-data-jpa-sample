1e205909932917afb88f821fa2c717246cf98d84
gmind7 <gmind7@gmail.com>
2014-01-04 17:41:45 +0900

    log4j2 설정 추가

--- spring-data-jpa/src/test/java/com/gmind7/bakery/AllTestRepository.java
+++ spring-data-jpa/src/test/java/com/gmind7/bakery/AllTestRepository.java
@@ -1,19 +1,12 @@
 package com.gmind7.bakery;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+import com.gmind7.bakery.config.AbstractApplicationTest;
 import com.gmind7.bakery.customer.TestCustomerRepository;
 import com.gmind7.bakery.employee.TestEmployeeRepository;
-import com.gmind7.bakery.handler.RoutingDataSource;
 import com.gmind7.bakery.office.TestOfficeRepository;
 import com.gmind7.bakery.order.TestOrderDetailRepository;
 import com.gmind7.bakery.order.TestOrderRepository;
@@ -32,17 +25,6 @@ import com.gmind7.bakery.product.TestProductRepository;
 	TestProductLineRepository.class,
 	TestProductRepository.class
 })
-public class AllTestRepository {
-	
-	@Autowired
-	private RoutingDataSource dataSource;
-	
-	@Before
-	public void initScriptSQL() throws SQLException {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("src/test/META-INF/mysqlsampledatabase.sql"));
-		Connection connection = dataSource.getConnection();
-		populator.populate(connection);
-	}
+public class AllTestRepository extends AbstractApplicationTest {
 	
 }
