878f1deed0e93db75b90f0dd6a56e372541f9b06
gmind7 <gmind7@gmail.com>
2014-01-20 12:21:48 +0900

    auditor를 security로 이동

--- spring-shared/src/test/java/com/gmind7/bakery/config/AbstractApplicationTest.java
+++ spring-shared/src/test/java/com/gmind7/bakery/config/AbstractApplicationTest.java
@@ -1,15 +1,10 @@
 package com.gmind7.bakery.config;
 
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-
-import com.gmind7.bakery.config.AppConfig;
-import com.gmind7.bakery.support.AuditorAwareOperations;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
@@ -17,11 +12,4 @@ import com.gmind7.bakery.support.AuditorAwareOperations;
 @ActiveProfiles("loc")
 public abstract class AbstractApplicationTest {
 	
-	@Autowired
-	private AuditorAwareOperations auditorAware;
-	
-	@Before
-	public void AuditorAware(){
-		auditorAware.setAuditor(1L);
-	}
 }
--- spring-shared/src/test/java/com/gmind7/bakery/config/AbstractIntegrationTest.java
+++ spring-shared/src/test/java/com/gmind7/bakery/config/AbstractIntegrationTest.java
@@ -7,16 +7,12 @@ import javax.annotation.Resource;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-
-import com.gmind7.bakery.config.AppConfig;
-import com.gmind7.bakery.support.AuditorAwareOperations;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
@@ -27,14 +23,6 @@ public abstract class AbstractIntegrationTest {
 	@Resource(name="defaultDatasource")
 	public org.apache.tomcat.jdbc.pool.DataSource defaultDatasource;
 
-	@Autowired
-	private AuditorAwareOperations auditorAware;
-	
-	@Before
-	public void AuditorAware(){
-		auditorAware.setAuditor(1L);
-	}
-	
 	@Before
 	public void 테스트데이터생성() throws SQLException {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
